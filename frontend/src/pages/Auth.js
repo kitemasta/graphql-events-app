f2360e1bfa178a03bede87cae0c542e12272e8d3
paul stepanenko <stepanenko.p@nimses.com>
2019-03-11 15:42:18 +0200

    initial frontend added

---
+++ frontend/src/pages/Auth.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AuthPage extends Component {
+  render() {
+    return (
+      <h1>Welcome to AuthPage!</h1>
+    )
+  }
+}
+
+export default AuthPage;
---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EventsPage extends Component {
+  render() {
+    return (
+      <div>
+        Events!
+      </div>
+    );
+  }
+}
+
+export default EventsPage;
