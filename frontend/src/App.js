01f168af22c11a1256a0e092bad0240b70d31c7a
paul stepanenko <stepanenko.p@nimses.com>
2019-03-11 18:05:09 +0200

    added header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import BookingsPage from './pages/Bookings';
 import EventsPage from './pages/Events';
+import MainNavigation from './components/Navigation/MainNavigation';
 
 import './App.css';
 
@@ -10,12 +11,17 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/events" component={EventsPage} />
-          <Route path="/bookings" component={BookingsPage} />
-        </Switch>
+        <Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </Fragment>
       </BrowserRouter>
     );
   }
