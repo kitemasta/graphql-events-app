14a3d68de23e48a129cc3dccb7369526980b281e
paul stepanenko <stepanenko.p@nimses.com>
2019-03-12 15:00:12 +0200

    added signup/login

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,11 +1,102 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
+import './Auth.css';
 
 class AuthPage extends Component {
-  render() {
-    return (
-      <h1>Welcome to AuthPage!</h1>
-    )
+  constructor(props) {
+    super(props);
+    this.emailEl = createRef();
+    this.passwordEl = createRef();
   }
+
+  state = {
+    isLogin: true
+  }
+
+  switchModeHandler = () => {
+    const { isLogin } = this.state;
+    this.setState({
+      isLogin: !isLogin
+    })
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault()
+    const email = this.emailEl.current.value;
+    const password = this.passwordEl.current.value;
+    const { isLogin } = this.state;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return
+    }
+
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `
+    };
+
+    if (!isLogin) {
+      requestBody = {
+        query: `
+          mutation {
+            createUser(userInput: {email: "${email}", password: "${password}"}) {
+              _id
+              email
+            }
+          }
+        `
+      }
+    }
+
+    fetch('http://localhost:8000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('failed');
+        }
+
+        return res.json();
+      })
+      .then(resData => {
+        console.log(resData);
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  };
+
+  render() {
+    const { isLogin } = this.state;
+    const formName = isLogin ? 'Sign up' : 'Log in';
+
+    return (
+      <form className="auth-form" onSubmit={this.submitHandler}>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="name" ref={this.emailEl} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" ref={this.passwordEl} />
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button" onClick={this.switchModeHandler}>Switch to {formName}</button>
+        </div>
+      </form>
+    )
+  };
 }
 
 export default AuthPage;
